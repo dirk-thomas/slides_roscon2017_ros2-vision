8e1e9bc7c04d8940a84eaf40dc11b40b4f1063de
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-09-13 09:22:19 -0700

    add script to export pdf using decktape

---
+++ pdf.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+docker run --rm -v `pwd`:/slides astefanutti/decktape http://www.dirk-thomas.net/slides_roscon2017_ros2-vision/2017-09-21_roscon_ros2-vision.html 2017-09-21_roscon_ros2-vision.pdf
